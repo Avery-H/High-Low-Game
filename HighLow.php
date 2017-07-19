32e18a46462b82ef60735d824ad1e4de3ab77d00
Avery Hankins <averyhankins@Averys-MacBook-Pro.local>
2017-07-19 11:26:49 -0500

    Added try counter

--- HighLow.php
+++ HighLow.php
@@ -1,16 +1,18 @@
 <?php 
 
 $ran = rand(1,100);
-
+$i = 0;
 do {
 	fwrite(STDOUT, "Pick a number!");
 	$userNum = trim(fgets(STDIN));
 	if ($userNum > $ran){
 		fwrite(STDOUT, "Too high! ");
+		$i++;
 	}
 	if ($userNum < $ran){
 		fwrite(STDOUT, "Too low! ");
+		$i++;
 	}
 } while($userNum != $ran);
 
-fwrite(STDOUT, "Good job!".PHP_EOL);
+fwrite(STDOUT, "Good job!(only took {$i} amount of times)".PHP_EOL);
